62fd221d72bb0e24e9c226ee8213804cf393cad9
Tyler J. Diaz <me@tylerdiaz.com>
2015-07-05 12:46:00 +0800

    :party: We have a server-side game tick!!

--- game-server.js.coffee
+++ game-server.js.coffee
@@ -1,1 +1,88 @@
-# a world unlocked
+Firebase = require('firebase')
+_ = require('underscore')
+
+Global =
+  firebaseRef: new Firebase("ws://local.firebaseio.com:5111")
+  tickRate: 200
+  ticks_per_morrow: 10
+  morrows_per_rabbit: 30
+
+GameState =
+  motherCycle: null
+  activeClans: []
+
+Global.firebaseRef.child("clans").on("child_added", (snapshot) ->
+  clan_data = snapshot.val()
+  GameState.activeClans.push(snapshot.key())
+)
+
+GameState.motherCycle = setInterval( ->
+  for clanKey in GameState.activeClans
+    Global.firebaseRef.child("clans/#{clanKey}").once("value", (snapshot) ->
+      Global.firebaseRef.child("clans/#{clanKey}")
+                        .set(new GameTicker(snapshot.val()).clan_data)
+    )
+, Global.tickRate)
+
+
+# ============= MEAT & POTATOES ===============
+
+class GameTicker
+  constructor: (@clan_data) ->
+    @clan_data.state_data.tick_counter = @clan_data.state_data.tick_counter + 1
+    @clan_data.timestamp = @morrowTick(@clan_data.state_data.tick_counter, @clan_data.timestamp)
+
+    if @isNewRabbit(@clan_data.timestamp)
+      @clan_data.current_policies = @clan_data.proposed_policies
+
+    if @isNewMorrow(@clan_data.state_data.tick_counter)
+      @tickUnitCosts(@isNewRabbit(@clan_data.timestamp))
+
+  isNewRabbit: (timestamp) ->
+    (timestamp % Global.morrows_per_rabbit) == 0
+
+  isNewMorrow: (tickCount) ->
+    tickCount % Global.ticks_per_morrow == 0
+
+  morrowTick: (tickCount, timestamp) ->
+    if @isNewMorrow(tickCount)
+      timestamp + 1
+    else
+      timestamp
+
+  tickUnitCosts: (isNewRabbit) ->
+    total_costs = {}
+
+    for unit in @clan_data.units
+      total_unit_costs = {}
+      for cost in unit['costs']
+        continue if cost.on_duty_contingency is true && unit.on_duty is false
+        continue if cost.frequency is 'rabbit' && isNewRabbit is false
+
+        resource = _.findWhere(@clan_data.resources, { resource: cost.resource_type })
+        if resource && resource.amount >= cost.resource_amount
+          total_unit_costs[cost.resource_type] = cost.resource_amount
+          # ...
+        else
+          total_unit_costs = false
+          # if you can't afford to pay the unit, make them unemployed
+          # if you can't afford to feed them, start a decay counter to run away/starvation
+          console.log 'unit cannot be afforded'
+
+      if total_unit_costs
+        total_costs = Object.keys(total_unit_costs).reduce((memo, key) ->
+          memo[key] = total_unit_costs[key] + (memo[key] || 0)
+          memo
+        , total_costs)
+
+    console.log "Total costs are: "+JSON.stringify(total_costs)
+
+        # {
+        #   resource_type: 'meal',
+        #   resource_amount: 2,
+        #   frequency: 'morrow',
+        #   on_duty_contingency: false,
+        # },
+
+  calculateBuildingCosts: (buildings) ->
+    console.log 'test'
--- game-server.js.js
+++ game-server.js.js
@@ -1,1 +1,109 @@
+var Firebase, GameState, GameTicker, Global, _;
 
+Firebase = require('firebase');
+
+_ = require('underscore');
+
+Global = {
+  firebaseRef: new Firebase("ws://local.firebaseio.com:5111"),
+  tickRate: 200,
+  ticks_per_morrow: 10,
+  morrows_per_rabbit: 30
+};
+
+GameState = {
+  motherCycle: null,
+  activeClans: []
+};
+
+Global.firebaseRef.child("clans").on("child_added", function(snapshot) {
+  var clan_data;
+  clan_data = snapshot.val();
+  return GameState.activeClans.push(snapshot.key());
+});
+
+GameState.motherCycle = setInterval(function() {
+  var clanKey, _i, _len, _ref, _results;
+  _ref = GameState.activeClans;
+  _results = [];
+  for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+    clanKey = _ref[_i];
+    _results.push(Global.firebaseRef.child("clans/" + clanKey).once("value", function(snapshot) {
+      return Global.firebaseRef.child("clans/" + clanKey).set(new GameTicker(snapshot.val()).clan_data);
+    }));
+  }
+  return _results;
+}, Global.tickRate);
+
+GameTicker = (function() {
+  function GameTicker(clan_data) {
+    this.clan_data = clan_data;
+    this.clan_data.state_data.tick_counter = this.clan_data.state_data.tick_counter + 1;
+    this.clan_data.timestamp = this.morrowTick(this.clan_data.state_data.tick_counter, this.clan_data.timestamp);
+    if (this.isNewRabbit(this.clan_data.timestamp)) {
+      this.clan_data.current_policies = this.clan_data.proposed_policies;
+    }
+    if (this.isNewMorrow(this.clan_data.state_data.tick_counter)) {
+      this.tickUnitCosts(this.isNewRabbit(this.clan_data.timestamp));
+    }
+  }
+
+  GameTicker.prototype.isNewRabbit = function(timestamp) {
+    return (timestamp % Global.morrows_per_rabbit) === 0;
+  };
+
+  GameTicker.prototype.isNewMorrow = function(tickCount) {
+    return tickCount % Global.ticks_per_morrow === 0;
+  };
+
+  GameTicker.prototype.morrowTick = function(tickCount, timestamp) {
+    if (this.isNewMorrow(tickCount)) {
+      return timestamp + 1;
+    } else {
+      return timestamp;
+    }
+  };
+
+  GameTicker.prototype.tickUnitCosts = function(isNewRabbit) {
+    var cost, resource, total_costs, total_unit_costs, unit, _i, _j, _len, _len1, _ref, _ref1;
+    total_costs = {};
+    _ref = this.clan_data.units;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      unit = _ref[_i];
+      total_unit_costs = {};
+      _ref1 = unit['costs'];
+      for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+        cost = _ref1[_j];
+        if (cost.on_duty_contingency === true && unit.on_duty === false) {
+          continue;
+        }
+        if (cost.frequency === 'rabbit' && isNewRabbit === false) {
+          continue;
+        }
+        resource = _.findWhere(this.clan_data.resources, {
+          resource: cost.resource_type
+        });
+        if (resource && resource.amount >= cost.resource_amount) {
+          total_unit_costs[cost.resource_type] = cost.resource_amount;
+        } else {
+          total_unit_costs = false;
+          console.log('unit cannot be afforded');
+        }
+      }
+      if (total_unit_costs) {
+        total_costs = Object.keys(total_unit_costs).reduce(function(memo, key) {
+          memo[key] = total_unit_costs[key] + (memo[key] || 0);
+          return memo;
+        }, total_costs);
+      }
+    }
+    return console.log("Total costs are: " + JSON.stringify(total_costs));
+  };
+
+  GameTicker.prototype.calculateBuildingCosts = function(buildings) {
+    return console.log('test');
+  };
+
+  return GameTicker;
+
+})();
